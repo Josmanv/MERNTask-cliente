e4052a951da065b47eef69b29c2c7fe365bd5358
josmanv <josem_lc@hotmail.com>
2021-06-06 12:05:22 +0200

    [add] form tareas - visual

---
+++ src/components/tareas/ListadoTarea.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ListadoTarea = () => {
+    return ( <h1>ListadoTarea</h1> );
+}
+ 
+export default ListadoTarea;
---
+++ src/components/tareas/Tarea.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Tarea = () => {
+    return ( <h1>Tarea</h1> );
+}
+ 
+export default Tarea;
