84071cd874bc0e34a442b8f0b95b50a782ad5e13
josmanv <josem_lc@hotmail.com>
2021-06-06 11:46:43 +0200

    [add] listando proyectos (solo front - datos estaticos)

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import NuevoProyecto from '../proyectos/NuevoProyecto';
+import ListadoProyectos from '../proyectos/ListadoProyectos';
 
 const Sidebar = () => {
     return ( 
         <aside>
             <h1>MERN<span>Tasks</span></h1>
             <NuevoProyecto />
-            <div className="proyectos"><h2>Tus Proyectos</h2></div>
+            <div className="proyectos">
+                <h2>Tus Proyectos</h2>
+                <ListadoProyectos />    
+            </div>
         </aside>
      );
 }
