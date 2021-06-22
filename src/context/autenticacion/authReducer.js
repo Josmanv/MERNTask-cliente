29929cbf2fcadf941c631ec73a0f8206fd2cd482
josmanv <josem_lc@hotmail.com>
2021-06-22 18:05:31 +0200

    envio token por headers

--- src/context/autenticacion/authReducer.js
+++ src/context/autenticacion/authReducer.js
@@ -9,14 +9,21 @@ import {
 
 const authReducer = (state, action) =>{
     switch(action.type){
-       case REGISTRO_EXITOSO:
+        case REGISTRO_EXITOSO:
             localStorage.setItem('token', action.payload.token);
             return{
                 ...state,
                 autenticado: true,
                 mensaje: null
             }
-       case REGISTRO_ERROR:
+        case OBTENER_USUARIO:
+            return{
+                ...state,
+              usuario: action.payload
+            }
+        case LOGIN_ERROR:
+        case REGISTRO_ERROR:
+            localStorage.removeItem('token');
             return{
                 ...state,
                 token: null,
--- src/context/autenticacion/authState.js
+++ src/context/autenticacion/authState.js
@@ -3,6 +3,7 @@ import authContext from './authContext';
 import authReducer from './authReducer';
 
 import clienteAxios from '../../config/axios';
+import tokenAuth from '../../config/tokenAuth';
 
 import {
     REGISTRO_EXITOSO,
@@ -34,6 +35,10 @@ const AuthState = props => {
                 type: REGISTRO_EXITOSO,
                 payload: respuesta.data
             });
+
+            // Obtener el usuario
+            usuarioAutenticado();
+
         } catch (error) {
             // console.log(error.response.data.msg);
             const alerta = {
@@ -44,6 +49,28 @@ const AuthState = props => {
             dispatch({
                 type: REGISTRO_ERROR,
                 payload: alerta
+            });
+        }
+    }
+
+    // retrona el usuario auntenticado
+    const usuarioAutenticado = async () => {
+        const token = localStorage.getItem('token');
+        if(token){
+            tokenAuth(token);
+        }
+
+        try {
+            const respuesta = await clienteAxios.get('/api/auth');
+            // console.log(respuesta);
+            dispatch({
+                type: OBTENER_USUARIO,
+                payload: respuesta.data.usuario
+            });
+        } catch (error) {
+            console.log(error.response);
+            dispatch({
+                type: LOGIN_ERROR
             });
         }
     }
